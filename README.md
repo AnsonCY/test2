dd0762982076189955d18703f12b9c7462eb40fc
HAHA <chanyuho.chan@gmail.com>
2026-04-15 15:40:15 +0800

    Revert "Update README with pull request instructions"
    This reverts commit 0fee61ea25bfcadaac4f135bc4e7a1de9d7b0aac.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Instruction
-How to use pull request
-https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-pull-requests
-https://gitbook.tw/chapters/github/pull-request
-https://hackmd.io/@judyyutong/understandPR
+# test2
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,15 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+// Source - https://stackoverflow.com/a/72382190
+// Posted by Sergio, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-14, License - CC BY-SA 4.0
+
+buildscript {
+    //...
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.9.0'
+
+        // ...
+    }
+}
